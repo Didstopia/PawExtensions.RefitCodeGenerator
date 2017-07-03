b5a437ede9bfffe594618cdb5b840fba6c521fe1
Pauli Jokela <pauli.jokela@didstopia.com>
2017-07-03 13:30:37 +0300

    Adding Travis and unit test support

---
+++ TestRefitCodeGenerator.js
@@ -0,0 +1,10 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const assert = require('chai').assert
+
+describe('Test Refit Code Generator', function () {
+  it('Dummy Test', function (done) {
+    assert.isTrue(true)
+    done()
+  })
+})
